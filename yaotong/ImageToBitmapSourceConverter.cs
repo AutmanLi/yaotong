ff1d1167d0b326a90479d2a1dfe49adc95e670e0
AutmanLi <autmanli123@gmail.com>
2018-11-16 22:09:22 +0800

    自定义imagebutton提交

---
+++ yaotong/ImageToBitmapSourceConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace yaotong
+{
+    [ValueConversion(typeof(Image), typeof(BitmapSource))]
+    public class ImageToBitmapSourceConverter : IValueConverter
+    {
+        [DllImport("gdi32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool DeleteObject(IntPtr value);
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+                return null;
+
+            var bitmap = new Bitmap((Image)value);
+            IntPtr bmpPt = bitmap.GetHbitmap();
+            BitmapSource bitmapSource =
+             System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                   bmpPt,
+                   IntPtr.Zero,
+                   Int32Rect.Empty,
+                   BitmapSizeOptions.FromEmptyOptions());
+
+            //freeze bitmapSource and clear memory to avoid memory leaks
+            bitmapSource.Freeze();
+            DeleteObject(bmpPt);
+
+            return bitmapSource;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- yaotong/imageButton.xaml.cs
+++ yaotong/imageButton.xaml.cs
@@ -16,18 +16,58 @@ using System.Windows.Shapes;
 namespace yaotong
 {
     /// <summary>
-    /// imageButton.xaml 的交互逻辑
+    /// Interaction logic for ImageButton.xaml
     /// </summary>
-    public partial class imageButton : UserControl
+    public partial class ImageButton : Button
     {
-
-        public event EventHandler click;
-        public imageButton(string imgUri)
+        public static readonly DependencyProperty ImageSourceProperty =
+            DependencyProperty.Register("ImageSource",
+            typeof(ImageSource), typeof(ImageButton), new PropertyMetadata(null));
+        public ImageSource ImageSource
         {
-            
-
+            get { return (ImageSource)GetValue(ImageSourceProperty); }
+            set { SetValue(ImageSourceProperty, value); }
         }
 
-       
+
+        public static readonly DependencyProperty ImageSourceHoverProperty =
+            DependencyProperty.Register("ImageSourceHover",
+            typeof(ImageSource), typeof(ImageButton), new PropertyMetadata(ImageSourceHoverCallback));
+        public ImageSource ImageSourceHover
+        {
+            get { return (ImageSource)GetValue(ImageSourceHoverProperty); }
+            set { SetValue(ImageSourceHoverProperty, value); }
+        }
+
+        private static void ImageSourceHoverCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((ImageButton)d).IsImageSourceHoverExist = e.NewValue != null;
+        }
+
+        public static readonly DependencyProperty IsImageSourceHoverExistProperty =
+            DependencyProperty.Register("IsImageSourceHoverExist",
+            typeof(bool), typeof(ImageButton), new PropertyMetadata(null));
+        public bool IsImageSourceHoverExist
+        {
+            get { return (bool)GetValue(IsImageSourceHoverExistProperty); }
+            set { SetValue(IsImageSourceHoverExistProperty, value); }
+        }
+
+
+        public ImageButton()
+        {
+            this.InitializeComponent();
+        }
+
+
+        protected override void OnMouseEnter(MouseEventArgs e)
+        {
+            base.OnMouseEnter(e);
+        }
+
+        protected override void OnMouseLeave(MouseEventArgs e)
+        {
+            base.OnMouseLeave(e);
+        }
     }
 }
